5a9fdd61c1a5cd29564bec6fc5ef41b26c30dfb4
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-01-12 18:04:42 -0300

    trabalhando com static

--- aplicacao-financeira/app/controllers/negociacao-controller.ts
+++ aplicacao-financeira/app/controllers/negociacao-controller.ts
@@ -19,8 +19,12 @@ export class NegociacaoController {
     this.negociacoesView.update(this.negociacoes);
   }
 
-  adiciona(): void {
-    const negociacao = this.criaNegociacao();
+  public adiciona(): void {
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
 
     if (!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView.update("negociações são feitas apenas em dias úteis.");
@@ -36,14 +40,6 @@ export class NegociacaoController {
       data.getDay() > DiasDaSemana.DOMINGO &&
       data.getDay() < DiasDaSemana.SABADO
     );
-  }
-
-  private criaNegociacao(): Negociacao {
-    const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ","));
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-    return new Negociacao(date, quantidade, valor);
   }
 
   private limparFormulario(): void {
