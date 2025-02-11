aa618c2e9a917a8398a0a16e50060bad87513681
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-02-11 19:58:41 -0300

    refatora a classe SavingsAccount

--- bytebank/app/savings-account/SavingsAccount.ts
+++ bytebank/app/savings-account/SavingsAccount.ts
@@ -5,8 +5,4 @@ export class SavingsAccount extends Account {
   constructor(client: ClientInterface, branch: number, balance: number) {
     super(client, branch, balance);
   }
-
-  withdrawAmmount(value: number) {
-    if (this.balance >= value) return (this.balance -= value);
-  }
 }
